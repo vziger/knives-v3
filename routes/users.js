89620d7743e7c98de62d8a7a42e803edd8e59e9d
le0n229 <leon229@yandex.ru>
2019-05-21 23:46:44 +0300

    rename result api

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,18 @@ router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
+
+router.post('/subscribe', async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    console.log(email);
+
+
+    res.status(200).send();
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+});
+
 module.exports = router;
