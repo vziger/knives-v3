e3348c6a44356409c6aeeca815e2de58dd14911a
vzhiger <lebedev_em@mail.ru>
2019-05-28 04:34:55 +0300

    hbs rules fix

--- routes/rules.js
+++ routes/rules.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get('/', (req, res,next) => {
     try {
-      // res.render('rules', { title: 'Правила участия', layout: 'layoutrules' });
-      res.render('rules');
+      res.render('rules', { title: 'Правила участия', layout: 'layoutrules' });
+      // res.render('rules');
       // , { title: 'Правила участия', layout: 'layoutrules' });
     } catch (error) {
       console.log(error.message);
