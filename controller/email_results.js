66b6215e6c84b32a8251514a3898330a66e98810
vzhiger <lebedev_em@mail.ru>
2019-06-01 21:11:05 +0300

    add all knife sum to letter

--- controller/email_results.js
+++ controller/email_results.js
@@ -22,105 +22,56 @@ async function mail_results(recepient, Player) {
   console.log('---- send results ----');
   console.log(Player);
 
-  let str_axe = digitsToHTML(Player.axe_4M);
-  let str_kn3 = digitsToHTML(Player.knife_3M);
-  let str_kn4 = digitsToHTML(Player.knife_4M);
-  let str_kn5 = digitsToHTML(Player.knife_5M);
+  let str_axe = digitsToHTML('Топоры, 4м', Player.axe_4M, Player.videoLink_1);
+  const str_kn3 = digitsToHTML('Ножи, 3м', Player.knife_3M, Player.videoLink_2);
+  const str_kn4 = digitsToHTML('Ножи, 4м', Player.knife_4M, Player.videoLink_3);
+  const str_kn5 = digitsToHTML('Ножи, 5м', Player.knife_5M, Player.videoLink_4);
 
-  let letter_body='<head>\
-	<style type="text/css">\
-    p{ margin-bottm:5px;}\
-    .container_flex{ display: flex; justify-content: flex-start;	}\
-    .digits{ width:20px; margin-top:2px; margin-right: 5px; \
-            border-bottom: 1px solid rgb(200, 200, 200, 0.5);	}\
-   	.label{ margin-top:2px;	width:120px; }\
-  	.discipline{ padding: 2px; 	margin:5px 0 7px 0;	}\
-   	.sum{ padding: 2px 8px 2px 8px;	background-color:rgb(200, 200, 200, 0.5);	}\
-  </style>\
-  </head>\
-  <body>\
-    <div style="font-family: Arial, Verdana, sans-serif;font-size:13px;padding:10px 0 0 10px;">\
-    <p>Здравствуйте!</p>\
-    <p>Вы отправили следующие результаты на&nbsp;онлайн-чемпионат<br/> метателей топоров и&nbsp;ножей «Метательные практики»:</p>\
-	  <div class="container_flex">\
-		  <div class="label">Имя</div>\
-		  <div>' + Player.first_name + '</div>\
-    </div> \
-    <div class="container_flex">\
-      <div class="label">Фамилия</div>\
-      <div>' + Player.last_name + '</div>\
-    </div> \
-    <div class="container_flex">\
-      <div class="label">Пол</div>\
-      <div>' + Player.gender + '</div>\
-    </div>\
-    <div class="container_flex">\
-      <div class="label">Клуб</div>\
-      <div>' + Player.club_name + '</div>\
-    </div>\
-    <div class="container_flex">\
-      <div class="label">Страна</div>\
-      <div>' + Player.country + '</div>\
-    </div>\
-    <div class="container_flex">\
-      <div class="label">Эл. почта:</div>\
-      <div><a href="/compose?To=' + Player.email + '" rel="noopener">' + Player.email + '</a></div>\
-    </div>\
-    <div class="discipline">\
-  		<div class="container_flex">\
-  			<div class="label">Топоры, 4м </div>\
-	  		<div class="container_flex">' + str_axe + '</div>\
-      </div>\
-      <div class="container_flex">\
-        <div class="label">Ссылка на видео</div>\
-        <div><a href="' + Player.videoLink_1 + 'target="_blank" rel="noopener noreferrer">' + 
-                          Player.videoLink_1 + '</a></div>\
-      </div>\
-    </div>\
-    <div class="discipline">\
-  		<div class="container_flex">\
-  			<div class="label">Ножи, 3м </div>\
-	  		<div class="container_flex">' + str_kn3 + '</div>\
-      </div>\
-      <div class="container_flex">\
-        <div class="label">Ссылка на видео</div>\
-        <div><a href="' + Player.videoLink_2 + 'target="_blank" rel="noopener noreferrer">' + 
-                          Player.videoLink_2 + '</a></div>\
-      </div>\
-    </div>\
-    <div class="discipline">\
-  		<div class="container_flex">\
-  			<div class="label">Ножи, 4м </div>\
-	  		<div class="container_flex">' + str_kn4 + '</div>\
-      </div>\
-      <div class="container_flex">\
-        <div class="label">Ссылка на видео</div>\
-        <div><a href="' + Player.videoLink_3 + 'target="_blank" rel="noopener noreferrer">' + 
-                          Player.videoLink_3 + '</a></div>\
-      </div>\
-    </div>\
-    <div class="discipline">\
-  		<div class="container_flex">\
-  			<div class="label">Ножи, 5м </div>\
-	  		<div class="container_flex">' + str_kn5 + '</div>\
-      </div>\
-      <div class="container_flex">\
-        <div class="label">Ссылка на видео</div>\
-        <div><a href="' + Player.videoLink_4 + 'target="_blank" rel="noopener noreferrer">' + 
-                          Player.videoLink_4 + '</a></div>\
-      </div>\
-    </div>\
-    <p>&nbsp;</p>\
-  	<p>Мы пришлём вам ссылку на&nbsp;опубликованные результаты.</p>\
-	  <p>Метательные практики<br/>\
-    <a href="http://knifethrowing.online" target="_blank" rel="noopener noreferrer">\
-              knifethrowing.online</a></p>\
-  </div>\  </body>';
+  let str_knives = str_kn3 + str_kn4 + str_kn5;
 
+  if (str_axe !== ''){
+    str_axe = '<div style="margin-top: 24px;">' + str_axe + '</div>';
+  }
+  if (str_knives !== '')
+  {
+    str_knives = '<div style="margin-top: 24px;">' + str_knives + '</div>';
+  }
 
-  let letter_body_2='<div style="font-family: Arial, Verdana, sans-serif;font-size:13px;padding:10px 0 0 10px;">\
-    <p style="margin-bottm:5px;">Здравствуйте!</p>\
-    <p style="margin-bottm:5px;">Вы отправили следующие результаты для участия в онлайн-чемпионате\
+  let allSum  = 0;
+  let number  = 0;
+  let allSumStr = '';
+  let sum_kn3 = mySumArray(Player.knife_3M);
+  let sum_kn4 = mySumArray(Player.knife_4M);
+  let sum_kn5 = mySumArray(Player.knife_5M);
+
+  if(sum_kn3 !== '' && !isNaN(sum_kn3)) {
+    number = number + 1;
+    allSum = allSum + sum_kn3;
+  }
+  if(sum_kn4 !== '' && !isNaN(sum_kn4)) {
+    number = number + 1;
+    allSum = allSum + sum_kn4;
+  }
+  if(sum_kn5 !== ''&& !isNaN(sum_kn5)) {
+    number = number + 1;
+    allSum = allSum + sum_kn5;
+  }
+  if (number > 1){
+    allSumStr ='<div style="padding: 2px; 	margin:0 0 7px 0;">\
+        <div style="display: flex; justify-content: flex-start;">\
+          <div style="margin-top:2px;	width:120px;">Сумма по ножам </div>\
+          <div style = "display: flex; justify-content: flex-start;">\
+            <div style="min-width: 250px"></div>\
+            <div style="margin-left: 5px; padding: 2px 8px 2px 8px;	\
+            background-color:rgb(200, 200, 200, 0.5);"><strong>' + allSum + '</strong></div>\
+          </div>\
+        </div>\
+      </div>';
+  }
+
+  let letter_body_2='<div style="font-family: Arial, Verdana, sans-serif;font-size:13px;padding:0 0 0 10px;">\
+    <p style="margin-bottom:5px;">Здравствуйте!</p>\
+    <p style="margin-bottom:5px;">Вы отправили следующие результаты для участия в онлайн-чемпионате\
       <br/> метателей топоров и&nbsp;ножей «Метательные практики»:</p>\
 	  <div style = "display: flex; justify-content: flex-start;">\
 		  <div style="margin-top:2px;	width:120px;">Имя</div>\
@@ -145,54 +96,10 @@ async function mail_results(recepient, Player) {
     <div style = "display: flex; justify-content: flex-start;">\
       <div style="margin-top:2px;	width:120px;">Эл. почта:</div>\
       <div><a href="/compose?To=' + Player.email + '" rel="noopener">' + Player.email + '</a></div>\
-    </div>\
-    <div style="padding: 2px; 	margin:5px 0 7px 0;">\
-  		<div style = "display: flex; justify-content: flex-start;">\
-  			<div style="margin-top:2px;	width:120px;">Топоры, 4м </div>\
-	  		<div style = "display: flex; justify-content: flex-start;">' + str_axe + '</div>\
-      </div>\
-      <div style = "display: flex; justify-content: flex-start;">\
-        <div style="margin-top:2px;	width:120px;">Ссылка на видео</div>\
-        <div><a href="' + Player.videoLink_1 + 'target="_blank" rel="noopener noreferrer">' + 
-                          Player.videoLink_1 + '</a></div>\
-      </div>\
-    </div>\
-    <div style="padding: 2px; 	margin:5px 0 7px 0;">\
-  		<div style = "display: flex; justify-content: flex-start;">\
-  			<div style="margin-top:2px;	width:120px;">Ножи, 3м </div>\
-	  		<div style = "display: flex; justify-content: flex-start;">' + str_kn3 + '</div>\
-      </div>\
-      <div style = "display: flex; justify-content: flex-start;">\
-        <div style="margin-top:2px;	width:120px;">Ссылка на видео</div>\
-        <div><a href="' + Player.videoLink_2 + 'target="_blank" rel="noopener noreferrer">' + 
-                          Player.videoLink_2 + '</a></div>\
-      </div>\
-    </div>\
-    <div style="padding: 2px; 	margin:5px 0 7px 0;">\
-  		<div style = "display: flex; justify-content: flex-start;">\
-  			<div style="margin-top:2px;	width:120px;">Ножи, 4м </div>\
-	  		<div style = "display: flex; justify-content: flex-start;">' + str_kn4 + '</div>\
-      </div>\
-      <div style = "display: flex; justify-content: flex-start;">\
-        <div style="margin-top:2px;	width:120px;">Ссылка на видео</div>\
-        <div><a href="' + Player.videoLink_3 + 'target="_blank" rel="noopener noreferrer">' + 
-                          Player.videoLink_3 + '</a></div>\
-      </div>\
-    </div>\
-    <div style="padding: 2px; 	margin:5px 0 7px 0;">\
-  		<div style = "display: flex; justify-content: flex-start;">\
-  			<div style="margin-top:2px;	width:120px;">Ножи, 5м </div>\
-	  		<div style = "display: flex; justify-content: flex-start;">' + str_kn5 + '</div>\
-      </div>\
-      <div style = "display: flex; justify-content: flex-start;">\
-        <div style="margin-top:2px;	width:120px;">Ссылка на видео</div>\
-        <div><a href="' + Player.videoLink_4 + 'target="_blank" rel="noopener noreferrer">' + 
-                          Player.videoLink_4 + '</a></div>\
-      </div>\
-    </div>\
-    <p style="margin-bottm:5px;">&nbsp;</p>\
-  	<p style="margin-bottm:5px;">Мы сообщим вам о победителях этапа и пришлём ссылку на турнирную таблицу.</p>\
-	  <p style="margin-bottm:5px;">Метательные практики<br/>\
+    </div>' + str_axe + str_knives + allSumStr +'\
+    <p style="margin-bottom:5px;">&nbsp;</p>\
+  	<p style="margin-bottom:5px;">10 июня мы сообщим о победителях этапа и пришлём ссылку на турнирную таблицу.</p>\
+	  <p style="margin-bottom:5px;">Метательные практики<br/>\
     <a href="http://knifethrowing.online" target="_blank" rel="noopener noreferrer">\
               knifethrowing.online</a></p>\
   </div>';
@@ -203,25 +110,25 @@ async function mail_results(recepient, Player) {
     from: 'Метательные практики <noreply.knifethrowing@gmail.com>', // sender address
     to: recepient, // list of receivers
     subject: 'Результаты приняты', // Subject line
-    text: `Здравствуйте!\n\n\
-    Вы отправили следующие результаты для участия в онлайн-чемпионате метателей топоров и \
-    ножей «Метательные практики»: \n\n Имя ${Player.first_name} \n\n\ 
-    Фамилия ${Player.last_name} \n\n\ Пол ${Player.gender} \n\n Клуб ${Player.club_name} \n\n\
-    Страна ${Player.country} \n\n\ Эл. почта ${Player.email} \n\n\
-    Мы сообщим вам о победителях этапа и пришлём ссылку на турнирную таблицу. \n\n\
-    Метательные практики\n\n\
-    http://knifethrowing.online`, // plain text body
+    // text: `Здравствуйте!\n\n\
+    // Вы отправили следующие результаты для участия в онлайн-чемпионате метателей топоров и \
+    // ножей «Метательные практики»: \n\n Имя ${Player.first_name} \n\n\ 
+    // Фамилия ${Player.last_name} \n\n\ Пол ${Player.gender} \n\n Клуб ${Player.club_name} \n\n\
+    // Страна ${Player.country} \n\n\ Эл. почта ${Player.email} \n\n\
+    // Мы сообщим вам о победителях этапа и пришлём ссылку на турнирную таблицу. \n\n\
+    // Метательные практики\n\n\
+    // http://knifethrowing.online`, // plain text body
     html: letter_body_2,
   // html: '<div style="font-family: Arial, Verdana, sans-serif; font-size:13px; padding:10px 0 0 10px; margin:0;">\
-  // <p style="margin-bottm:5px;">Здравствуйте!</p>\
-  // <p style="margin-bottm:5px;">Вы отправили следующие результаты \
+  // <p style="margin-bottom:5px;">Здравствуйте!</p>\
+  // <p style="margin-bottom:5px;">Вы отправили следующие результаты \
   // на&nbsp;онлайн-чемпионат<br/> метателей топоров и&nbsp;ножей «Метательные практики»:</p>\
-  // <p style="margin-bottm:5px;">Имя '   + Player.first_name + ' \
-  // <p style="margin-bottm:5px;">Фамилия ' + Player.last_name + '\
-  // <p style="margin-bottm:5px;">Пол ' + Player.gender + '\
-  // <p style="margin-bottm:5px;">Клуб ' + Player.club_name + '\
-  // <p style="margin-bottm:5px;">Страна ' + Player.country + '\
-  // <p style="margin-bottm:5px;">Эл. почта ' + Player.email + '<a href="/compose?To=' + Player.email + '" rel="noopener noreferrer">' + Player.email + '</a>\
+  // <p style="margin-bottom:5px;">Имя '   + Player.first_name + ' \
+  // <p style="margin-bottom:5px;">Фамилия ' + Player.last_name + '\
+  // <p style="margin-bottom:5px;">Пол ' + Player.gender + '\
+  // <p style="margin-bottom:5px;">Клуб ' + Player.club_name + '\
+  // <p style="margin-bottom:5px;">Страна ' + Player.country + '\
+  // <p style="margin-bottom:5px;">Эл. почта ' + Player.email + '<a href="/compose?To=' + Player.email + '" rel="noopener noreferrer">' + Player.email + '</a>\
   // <p>Мы пришлём вам ссылку на&nbsp;опубликованные результаты</p>\
   // <p>Метательные практики<br/>\
   //   <a href="http://knifethrowing.online">knifethrowing.online</a></p></div>// // html body
@@ -235,20 +142,41 @@ async function mail_results(recepient, Player) {
 // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-function digitsToHTML(myArray){
+function digitsToHTML(pattern, myArray, videoLink){
   let str = '';
   let sum = 0;
-  if(myArray[0]!==''){
-    for(let i=0;i<10;i++){
+
+  if(myArray[0] !== ''){
+  //   if(pattern=='Топоры, 4м') {
+  //     str = str + '<div style="margin-top: 24px;"> <div style="padding: 2px; 	margin:5px 0 7px 0;">';
+  //   }
+      
+  //   else 
+    str = str + '<div style="padding: 2px; 	margin:5px 0 7px 0;">';
+    
+    str = str + '<div style = "display: flex; justify-content: flex-start;">\
+      <div style="margin-top:2px;	width:120px;">' + pattern + '</div>\
+      <div style = "display: flex; justify-content: flex-start;">';
+
+    for(let i=0; i<10; i++){
       str = str + '<div style="width:20px; margin-top:2px; margin-right: 5px; \
       border-bottom: 1px solid rgb(200, 200, 200, 0.5);">' + myArray[i] + '</div>';
       sum = sum + parseInt(myArray[i]);
     }
-    str = str + '<div style="padding: 2px 8px 2px 8px;	\
-    background-color:rgb(200, 200, 200, 0.5);"><strong>' + sum + '</strong></div>';
+    str = str + '<div style="margin-left: 5px; padding: 2px 8px 2px 8px;	\
+    background-color:rgb(200, 200, 200, 0.5);"><strong>' + sum + '</strong></div>\
+    </div>\
+      </div>\
+      <div style = "display: flex; justify-content: flex-start;">\
+        <div style="margin-top:2px;	width:120px;">Ссылка на видео</div>\
+        <div><a href="' + videoLink + 'target="_blank" rel="noopener noreferrer">' + 
+                          videoLink + '</a></div>\
+      </div>\
+    </div>';
+    
   }
   else {
-    str = '—';
+    str = '';
   }
   return str;
 }
@@ -261,5 +189,15 @@ function capitalizeFirstLetter(str)
     return uppercaseFirstLetter+stringWithoutFirstLetter;
 }
 
+function mySumArray(arr){
+  if (arr.length>0){
+    let sum = 0;
+    for(let i=0; i<10; i++){
+      sum = sum + parseInt(arr[i]);;
+    }
+    return sum;
+  }
+  return '';
+}
 
 module.exports = mail_results;
