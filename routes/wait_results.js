fab3acd63a506282517689271000fa831b86af54
vzhiger <lebedev_em@mail.ru>
2019-06-03 20:01:40 +0300

    try to fix zalip photo on WR_#1

--- routes/wait_results.js
+++ routes/wait_results.js
@@ -7,7 +7,7 @@ router.get('/', (req, res,next) => {
     try {
       res.render('index', { title: 'Правила участия', 
       img_dates_src:'/images/dates_wait_result.svg',
-      text_send_results:'Прием результатов откроем в\u00A0следующем этапе.',
+      text_send_results:'Приём результатов откроем в\u00A0следующем этапе.',
       layout: 'layout_counting_results' });
     } catch (error) {
       console.log(error.message);
