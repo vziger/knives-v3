7966d009fd523a12c286a0d003c528597cfee0af
vzhiger <lebedev_em@mail.ru>
2019-06-03 18:44:10 +0300

    text on rules page

--- routes/wait_results.js
+++ routes/wait_results.js
@@ -7,10 +7,7 @@ router.get('/', (req, res,next) => {
     try {
       res.render('index', { title: 'Правила участия', 
       img_dates_src:'/images/dates_wait_result.svg',
-      text_send_results:'Второй этап окончен. \
-      Откроем приём результатов в\u00A0даты проведения следующего этапа. \
-      За\u00A0каждый\u00A0бросок начисляется столько баллов, сколько указано на\u00A0мишени, \
-      куда попал топор или\u00A0нож.',
+      text_send_results:'Откроем прием результатов в\u00A0следующем этапе.',
       layout: 'layout_counting_results' });
     } catch (error) {
       console.log(error.message);
