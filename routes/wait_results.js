eae74116463b302d08d4e10bd87ed3479591aff2
vzhiger <lebedev_em@mail.ru>
2019-06-04 04:33:52 +0300

    new html for rules

--- routes/wait_results.js
+++ routes/wait_results.js
@@ -7,7 +7,10 @@ router.get('/', (req, res,next) => {
     try {
       res.render('index', { title: 'Правила участия', 
       img_dates_src:'/images/dates_wait_result.svg',
-      text_send_results:'Приём результатов откроем в\u00A0следующем этапе.',
+      text_send_results:'Второй этап закончился. Следующий начнётся в сентябре.\
+      Чтобы подготовиться, прочитайте правила участия, выберите топор и три ножа, \
+      распечатайте десять мишеней и зарядите камеру. Участвуйте во всех этапах \
+      турнира и отслеживайте свой прогресс в конце года.',
       layout: 'layout_counting_results' });
     } catch (error) {
       console.log(error.message);
