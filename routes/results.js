e2ac5d63fdaba7622b6ef62d42905da2c364fc65
vzhiger <lebedev_em@mail.ru>
2019-06-07 16:09:43 +0300

    fix result page with Hat

--- routes/results.js
+++ routes/results.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res,next) => {
     try {
-      res.render('results', { title: 'Результаты' });
+      res.render('results', { title: 'Результаты второго этапа' });
     } catch (error) {
       console.log(error.message);
       next(error);
