d9ca398241d26938ac16704e2885e3239000472a
vzhiger <lebedev_em@mail.ru>
2019-06-13 21:44:03 +0300

    new rules

--- routes/rules.js
+++ routes/rules.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res,next) => {
     try {
-      res.render('rules', { title: 'Правила участия', layout: 'layoutrules' });
+      res.render('new_rules', { title: 'Правила участия', layout: 'layout_newrules' });
     } catch (error) {
       console.log(error.message);
       next(error);
